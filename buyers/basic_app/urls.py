7d5049aa0c1b6554a13ea01bfd837d658cdab8af
shrim006 <Manish.x.shringari.-nd@disney.com>
2018-10-16 01:46:06 +0530

    added views

--- buyers/basic_app/urls.py
+++ buyers/basic_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$',views.CustomersListView.as_view(),name='customers_list'),
     url(r'^about/$',views.AboutView.as_view(),name='about'),
-    url(r'^appraisal/$',views.AppraisalView.as_view(),name='about'),
+    url(r'^appraisal/$',views.AppraisalView.as_view(),name='appraisal'),
     url(r'^inventory/$', views.InventoryView.as_view(), name='inventory'),
     url(r'^purchase/$', views.PurchaseView.as_view(), name='purchase'),
     url(r'^customers/(?P<pk>\d+)$', views.CustomersDetailView.as_view(), name='customers_detail'),
